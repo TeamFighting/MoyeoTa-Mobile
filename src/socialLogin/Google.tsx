cbd6f1b83145d53a20ecd7c3e768069e32d0e361
wldnjs7064 <wldnjs7064@naver.com>
2023-09-12 19:15:33 +0900

    fix: 구글 로그인 구현 webview 사용 시 오류 해결

--- src/socialLogin/Google.tsx
+++ src/socialLogin/Google.tsx
@@ -7,16 +7,17 @@ import { useNavigation } from "@react-navigation/native";
 
 function Google() {
   const navigation = useNavigation();
+  //Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36
 
+  const customUserAgent = "customUserAgent";
   return (
     <View style={{ flex: 1, marginTop: 24, backgroundColor: "#fff" }}>
       <WebView
         style={{ flex: 1 }}
         source={{
-          uri: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID}
-&redirect_uri=${process.env.EXPO_PUBLIC_REDIRECT_URI}&response_type=code&scope=email%20profile`,
+          uri: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.EXPO_PUBLIC_REDIRECT_URI}&response_type=code&scope=email%20profile`,
         }}
-        userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+        userAgent={customUserAgent}
         originWhitelist={["*"]}
         injectedJavaScript={INJECTED_JAVASCRIPT}
         javaScriptEnabled
