0ddb1853dbb6eef7157bc8a2fe0094bdcfa943d6
hyorish03 <hyorish2001@gmail.com>
2023-09-18 02:25:10 +0900

    Feat: App.tsx 문구 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ alignItems: 'center', textAlign: 'center' }}>
-      <h1 style={{ textAlign: 'center' }}> testtest</h1>
+      <h1 style={{ textAlign: 'center' }}> 로그인 되었습니다.</h1>
     </div>
   );
 }
