698537f739a8acd7b5626b9272de2bd94f0b7794
hyorish03 <hyorish2001@gmail.com>
2023-09-18 15:11:00 +0900

    Feat: react-kakao-maps-sdk를 이용해 맵 띄우기

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import React from 'react';
+import { Map, useKakaoLoader } from 'react-kakao-maps-sdk';
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    kakao: any;
+  }
+}
 function App() {
+  console.log('env', import.meta.env.VITE_KAKAOMAP_JAVASCRIPT_APP_KEY);
+
+  const [loading, error] = useKakaoLoader({
+    appkey: '9390a5f4210490b4f089219e30f5cf0b',
+  });
   return (
-    <div style={{ alignItems: 'center', textAlign: 'center' }}>
-      <h1 style={{ textAlign: 'center' }}> 로그인 되었습니다.</h1>
-    </div>
+    <Map
+      className="myMap"
+      style={{ width: '500px', height: '500px' }}
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      level={3}
+    ></Map>
   );
 }
 
--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,5 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { MutableRefObject, useEffect, useRef } from 'react';
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    kakao: any;
+  }
+}
+
 function MainPage() {
-  return <div>test cd</div>;
+  const mapRef = useRef<HTMLElement | null>(null);
+
+  const initMap = () => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new window.kakao.maps.LatLng(37.483034, 126.902435),
+      level: 2,
+    };
+
+    const map = new window.kakao.maps.Map(container as HTMLElement, options);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (mapRef as MutableRefObject<any>).current = map;
+  };
+
+  useEffect(() => {
+    kakao.maps.load(() => initMap());
+  }, [mapRef]);
+
+  return <div id="map" style={{ width: '500px', height: '400px' }}></div>;
 }
 
 export default MainPage;
