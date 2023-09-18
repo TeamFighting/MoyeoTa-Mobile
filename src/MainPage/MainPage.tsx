5d86bb3201cece4b50acc4caf2b33475fe282884
hyorish03 <hyorish2001@gmail.com>
2023-09-18 16:12:04 +0900

    Chore: 파일 이름 정리

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,28 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { MutableRefObject, useEffect, useRef } from 'react';
-
-
+import Map from '../Map/Map';
 
 function MainPage() {
-  const mapRef = useRef<HTMLElement | null>(null);
-
-  const initMap = () => {
-    const container = document.getElementById('map');
-    const options = {
-      center: new window.kakao.maps.LatLng(37.483034, 126.902435),
-      level: 2,
-    };
-
-    const map = new window.kakao.maps.Map(container as HTMLElement, options);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (mapRef as MutableRefObject<any>).current = map;
-  };
-
-  useEffect(() => {
-    kakao.maps.load(() => initMap());
-  }, [mapRef]);
-
-  return <div id="map" style={{ width: '500px', height: '400px' }}></div>;
+  return (
+    <div>
+      <Map />
+    </div>
+  );
 }
 
 export default MainPage;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
-import MainPage from "./MainPage/MainPage.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import MainPage from './MainPage/Mainpage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
   {
-    path: "/mainPage",
+    path: '/mainPage',
     element: <MainPage />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
