8320be4ba37b0c2551ffa7b52448a22cd2da8888
hyorish03 <hyorish2001@gmail.com>
2023-09-18 16:02:33 +0900

    Feat: App.tsx 문구 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { MutableRefObject, useEffect, useRef } from 'react';
+import React from 'react';
 
 function App() {
-  console.log('App', import.meta.env.VITE_KAKAOMAP_JAVASCRIPT_APP_KEY);
-  const mapRef = useRef<HTMLElement | null>(null);
-
-  const initMap = () => {
-    const container = document.getElementById('map');
-    const options = {
-      center: new kakao.maps.LatLng(37.483034, 126.902435),
-      level: 2,
-    };
-
-    const map = new kakao.maps.Map(container as HTMLElement, options);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (mapRef as MutableRefObject<any>).current = map;
-  };
-
-  useEffect(() => {
-    kakao.maps.load(() => initMap());
-  }, [mapRef]);
-
-  return <div id="map" style={{ width: '500px', height: '400px' }}></div>;
+  return <div>로그인 성공</div>;
 }
 
 export default App;
