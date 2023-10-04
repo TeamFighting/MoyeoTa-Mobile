9b9bd443572861e40d517b7c6452aaee9ae85455
wldnjs7064 <wldnjs7064@naver.com>
2023-10-04 18:33:24 +0900

    fix: 폰트 로딩 에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { useFonts } from 'expo-font';
-import Onboarding from './components/Onboarding';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignIn from './Screens/SignIn/SignIn';
-import axios from 'axios';
-import { SplashScreen } from 'expo-router';
-import { useEffect, useState } from 'react';
-import * as Font from 'expo-font';
-import MainPage from './Screens/MainPage/MainPage';
-import Kakao from './Screens/SignIn/SocialLogin/Kakao';
-import Naver from './Screens/SignIn/SocialLogin/Naver';
-import Google from './Screens/SignIn/SocialLogin/Google';
-=======
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { useFonts } from "expo-font";
@@ -31,17 +14,16 @@ import { SplashScreen } from "expo-router";
 import { useEffect, useState } from "react";
 import * as Font from "expo-font";
 import Google from "./Screens/SignIn/socialLogin/Google";
->>>>>>> db99cb58f7dbba2b016a44e2706436e27d50369f
 
-const loadFonts = () => {
-  return Font.loadAsync({
-    Pretendard: require('../assets/font/Pretendard-Medium.otf'),
-    PretedardBlack: require('../assets/font/Pretendard-Black.otf'),
-    PretendardBold: require('../assets/font/Pretendard-Bold.otf'),
-    PretendardExtraBold: require('../assets/font/Pretendard-ExtraBold.otf'),
-    PretendardLight: require('../assets/font/Pretendard-Light.otf'),
-    PretendardRegular: require('../assets/font/Pretendard-Regular.otf'),
-    PretendardSemiBold: require('../assets/font/Pretendard-SemiBold.otf'),
+const loadFonts = async () => {
+  await Font.loadAsync({
+    Pretendard: require("../assets/font/Pretendard-Medium.otf"),
+    PretedardBlack: require("../assets/font/Pretendard-Black.otf"),
+    PretendardBold: require("../assets/font/Pretendard-Bold.otf"),
+    PretendardExtraBold: require("../assets/font/Pretendard-ExtraBold.otf"),
+    PretendardLight: require("../assets/font/Pretendard-Light.otf"),
+    PretendardRegular: require("../assets/font/Pretendard-Regular.otf"),
+    PretendardSemiBold: require("../assets/font/Pretendard-SemiBold.otf"),
   });
 };
 
@@ -65,31 +47,16 @@ export default function App() {
     return null;
   }
 
-  // const [fontsLoaded] = useFonts({
-  //   Pretendard: require("../assets/font/Pretendard-Medium.otf"),
-  //   PretedardBlack: require("../assets/font/Pretendard-Black.otf"),
-  //   PretendardBold: require("../assets/font/Pretendard-Bold.otf"),
-  //   PretendardExtraBold: require("../assets/font/Pretendard-ExtraBold.otf"),
-  //   PretendardLight: require("../assets/font/Pretendard-Light.otf"),
-  //   PretendardRegular: require("../assets/font/Pretendard-Regular.otf"),
-  //   PretendardSemiBold: require("../assets/font/Pretendard-SemiBold.otf"),
-  // });
-  // if (!fontsLoaded) return null;
-
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-<<<<<<< HEAD
-      <Stack.Navigator initialRouteName="SignIn">
-=======
       <Stack.Navigator initialRouteName="Slogan">
         <Stack.Screen
           name="Slogan"
           component={Slogan}
           options={{ headerShown: false }}
         />
->>>>>>> db99cb58f7dbba2b016a44e2706436e27d50369f
         <Stack.Screen
           name="Onboarding"
           component={Onboarding}
@@ -116,13 +83,8 @@ export default function App() {
           options={{ headerShown: false }}
         />
         <Stack.Screen
-<<<<<<< HEAD
-          name="MainPage"
-          component={MainPage}
-=======
           name="Guide"
           component={Guide}
->>>>>>> db99cb58f7dbba2b016a44e2706436e27d50369f
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
@@ -133,8 +95,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
