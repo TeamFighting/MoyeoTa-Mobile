c6d98bc38d2434947d1b63776e3b24d66e972799
wldnjs7064 <wldnjs7064@naver.com>
2023-10-04 16:21:07 +0900

    feat: 학교 이름 api 호출

--- src/Screens/SignIn/schoolLogin/schoolList.tsx
+++ src/Screens/SignIn/schoolLogin/schoolList.tsx
@@ -6,12 +6,43 @@ import {
   View,
   Pressable,
   TextInput,
+  FlatList,
 } from "react-native";
 import LeftArrow from "../../../../assets/svg/leftArrow.svg";
-import QuestionMark from "../../../../assets/svg/questionMark.svg";
 import { colors } from "../../../styles/color";
+import axios from "axios";
 
 function SchoolList({ route, navigation }: { route: any; navigation: any }) {
+  const [schoolData, setSchoolData] = useState<string[]>([]); // 학교 이름을 저장할 배열
+
+  useEffect(() => {
+    const apiUrl = `http://www.career.go.kr/cnet/openapi/getOpenApi.json?apiKey=${process.env.SCHOOL_API_KEY}&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        const responseData = response.data;
+
+        if (
+          responseData &&
+          responseData.dataSearch &&
+          responseData.dataSearch.content
+        ) {
+          const schoolData = responseData.dataSearch.content;
+
+          const schoolNames = schoolData.map(
+            (school: any) => school.schoolName
+          );
+          console.log(schoolNames);
+        } else {
+          console.error("잘못된 형식입니다.");
+        }
+      })
+      .catch((error) => {
+        console.error("API 요청 중 오류 발생:", error);
+      });
+  }, []);
+
   return (
     <View style={{ width: "100%", height: "100%", backgroundColor: "#fff" }}>
       <View
@@ -38,6 +69,14 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingTop: windowHeight * 0.05,
     paddingLeft: "7%",
+  },
+  schoolListContainer: {
+    marginTop: 20,
+    marginLeft: "7%",
+  },
+  schoolItem: {
+    fontSize: 16,
+    marginBottom: 10,
   },
 });
 
